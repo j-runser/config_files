15e1e425b6496f295a1ab5e361049583ca097ebb
j-runser <joseph.runser@udri.udayton.edu>
2019-06-07 14:25:10 -0400

    Added config for fish.

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval (eval /home/jrunser/anaconda2/bin/conda "shell.fish" "hook" $argv)
+# <<< conda initialize <<<
+
